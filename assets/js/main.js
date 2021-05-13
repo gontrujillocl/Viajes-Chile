8a77c4b7f6409d0dca1a87ff68e7b06d0900eb19
Gonzalo <gonzalo@truall.com>
2021-05-13 19:38:54 -0400

    Navbar listo, Typeform y footer.

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+//Typeform
+(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, 
+gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; 
+if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; 
+q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) 
+} 
+})
+() 
+
+//Scroll Fixed Color fondo
+$(window).scroll(function(){
+if ($("#menu").offset().top > 56){
+    $("#menu").addClass("bg-info");
+}else {
+    $("#menu").removeClass("bg-info");
+}
+});
+
+$('body').scrollspy({target: ".navbar", offset: 50});
+$("#myNavbar a").on('click', 
+function(event) {
+if (this.hash !== "") {
+  event.preventDefault();
+  var hash = this.hash;
+  $('html, body').animate({
+  scrollTop: $(hash).offset().top}, 800, function(){
+  window.location.hash = hash;
+  });
+}
+});
